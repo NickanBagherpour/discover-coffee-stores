14911bd150d734efc8c7d0aaf5e0312d453d3d8f
Nickan <Nickan.bagherpour@gmail.com>
2022-09-18 09:07:21 +0430

    feat: useTrackLocation hook, get user location, ...

--- pages/index.js
+++ pages/index.js
@@ -4,6 +4,7 @@ import Image from "next/image";
 import Banner from "../components/banner";
 import Card from "../components/card";
 
+import useTrackLocation from "../hooks/use-track-location";
 import {fetchCoffeeStores} from "../lib/coffee-stores";
 import styles from "../styles/Home.module.css";
 
@@ -19,9 +20,16 @@ export async function getStaticProps(context) {
 }
 
 const Home = (props) => {
+
+    const {handleTrackLocation, latLong, locationErrorMsg, isFindingLocation} =
+        useTrackLocation();
+
+    console.log({latLong, locationErrorMsg});
+
     const handleOnBannerBtnClick = () => {
-        console.log("hi banner button");
+        handleTrackLocation();
     };
+
     return (
         <div className={styles.container}>
             <Head>
@@ -32,32 +40,36 @@ const Home = (props) => {
 
             <main className={styles.main}>
                 <Banner
-                    buttonText="View stores nearby"
+                    buttonText={isFindingLocation ? "Locating..." : "View stores nearby"}
                     handleOnClick={handleOnBannerBtnClick}
                 />
+                {locationErrorMsg && <p>Something went wrong: {locationErrorMsg}</p>}
+
                 <div className={styles.heroImage}>
                     <Image src="/static/hero-image.png" width={700} height={400}/>
                 </div>
-                {props.coffeeStores.length > 0 && (
-                    <>
-                        <h2 className={styles.heading2}>Toronto stores</h2>
-                        <div className={styles.cardLayout}>
-                            {props.coffeeStores.map((coffeeStore) => {
-                                return (
-                                    <Card
-                                        key={coffeeStore.id}
-                                        name={coffeeStore.name}
-                                        imgUrl={
-                                            coffeeStore.imgUrl || ""
-                                        }
-                                        href={`/coffee-store/${coffeeStore.id}`}
-                                        className={styles.card}
-                                    />
-                                );
-                            })}
-                        </div>
-                    </>
-                )}
+                <div className={styles.sectionWrapper}>
+                    {props.coffeeStores.length > 0 && (
+                        <>
+                            <h2 className={styles.heading2}>Toronto stores</h2>
+                            <div className={styles.cardLayout}>
+                                {props.coffeeStores.map((coffeeStore) => {
+                                    return (
+                                        <Card
+                                            key={coffeeStore.id}
+                                            name={coffeeStore.name}
+                                            imgUrl={
+                                                coffeeStore.imgUrl || ""
+                                            }
+                                            href={`/coffee-store/${coffeeStore.id}`}
+                                            className={styles.card}
+                                        />
+                                    );
+                                })}
+                            </div>
+                        </>
+                    )}
+                </div>
             </main>
         </div>
     );
